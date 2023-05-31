c30e1b342a4e2b18e6888090ae17700a548b827f
Ravi Chandra reddy <ravic6630@gmail.com>
2023-05-31 16:12:16 -0500

    next commit

---
+++ Day2/src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    private String name;
+
+    // Getter method to return the value of the name variable
+    public String getName() {
+        return name;
+    }
+
+    // Setter method to update the value of the name variable
+    public void setName(String newName) {
+        name = newName;
+    }
+}
+
