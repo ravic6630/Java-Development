423b39b4f98363d78b4903208256d8d9aff57714
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-02 14:34:59 -0500

    commit

--- Day3/src/Animal.java
+++ Day3/src/Animal.java
@@ -1,32 +1,32 @@
-class Animal
+class Animal1
 {
     void eat(){
         System.out.println("Animals Eat");
     }
 }
-class herbivores extends Animal{
+class herbivores extends Animal1{
     void eat(){
         System.out.println("Herbivores Eat Plants");
     }
 }
-class omnivores extends Animal{
+class omnivores extends Animal1{
     void eat(){
         System.out.println("Omnivores Eat plants and meat");
     }
 
 }
-class carnivores extends Animal{
+class carnivores extends Animal1{
     void eat(){
         System.out.println("Carnivores Eat Meat");
     }
 }
 
-class Animal1 {
+class Animal {
     public static void main(String[] args) {
-        Animal A= new Animal();
-        Animal h= new herbivores();
-        Animal o= new omnivores();
-        Animal c= new carnivores();
+        Animal1 A= new Animal1();
+        Animal1 h= new herbivores();
+        Animal1 o= new omnivores();
+        Animal1 c= new carnivores();
         A.eat();
         h.eat();
         o.eat();
