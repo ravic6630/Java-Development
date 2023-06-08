1ad71dbc78211f714f68f0b069aa935ea189c9e9
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-08 11:43:22 -0500

    commit

--- Java/src/com/week2/Tue/Reverse.java
+++ Java/src/com/week2/Tue/Reverse.java
@@ -2,7 +2,7 @@ package com.week2.Tue;
 import java.util.*;
 
 public class Reverse {
-    public static void main(String[] args) {
+    public static void main(String[] argscd ) {
         int[] a={1,2,3,4,5,6,8};
         int i=0, j=a.length-1,temp;
         while (i<j){
