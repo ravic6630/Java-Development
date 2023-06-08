a978d76d196b52958ec98a664eee0e575355c36d
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-08 15:15:38 -0500

    update

---
+++ src/com/week1/day1/Student.java
@@ -0,0 +1,13 @@
+package com.week1.day1;
+
+public class Student {
+    private String name; // state variable
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+}
