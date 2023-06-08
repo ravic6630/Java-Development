3970236629919af932e99225aa834cab7ab6f458
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-08 14:46:47 -0500

    commit

---
+++ Java/src/com/week1/day1/Animal.java
@@ -0,0 +1,8 @@
+package com.week1.day1;
+
+class Animal {
+    void eat(){
+        System.out.println("Just Eating");
+
+    }
+}
---
+++ Java/src/com/week1/day3/Final.java
@@ -0,0 +1,17 @@
+package com.week1.day3;
+
+final class Final {
+    final int Speedlimit=75;
+
+
+    final void speed(){
+        System.out.println("Max Speed Limit is "+ Speedlimit);
+    }
+
+    public static void main(String[] args) {
+        Final s=new Final();
+        s.speed();
+    }
+
+
+}
