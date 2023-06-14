a014272c1598c00d443cd5bc9d5b896c1067f51f
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-14 12:36:19 -0500

    commit

---
+++ src/com/week3/tue/Mythread.java
@@ -0,0 +1,24 @@
+package com.week3.tue;
+
+public class Mythread implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println( "Thread" +" "+ i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Mythread numberPrinter = new Mythread();
+
+        Thread thread = new Thread(numberPrinter);
+       // System.out.println("thread");
+        thread.start();
+
+    }
+}
