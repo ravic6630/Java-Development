fb0c3a307740c2c82cd1f3534b40ccb94f565aae
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-19 13:29:04 -0500

    Final update

---
+++ src/com/week3/fri/StringSorting.java
@@ -0,0 +1,20 @@
+package com.week3.fri;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class StringSorting {
+    public static void stringSorting(String[] strings) {
+
+        Arrays.sort(strings, Comparator.comparingInt(String::length)
+                .thenComparing((a, b) -> b.charAt(a.length() - 1) - a.charAt(a.length() - 1)));
+
+        Arrays.stream(strings).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        String[] strings1 =  {"oppenheimmer", "interstellar", "dunkirk", "prestige" , "inception" , "Tenet"};
+        System.out.println("\nSTRING SORTING");
+        StringSorting.stringSorting(strings1);
+    }
+}
