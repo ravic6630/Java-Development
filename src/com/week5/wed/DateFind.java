dad9914a90ade8bee92baafeb575427c562231da
Ravi Chandra reddy <ravic6630@gmail.com>
2023-06-29 16:54:02 -0500

    second commit

---
+++ src/com/week5/wed/DateFind.java
@@ -0,0 +1,30 @@
+package com.week5.wed;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateFind {
+    public static void main(String[] args) {
+        // Create a list of day
+        List<LocalDate> day = new ArrayList<>();
+        day.add(LocalDate.parse("2020-06-15"));
+        day.add(LocalDate.parse("2021-06-25"));
+        day.add(LocalDate.parse("2021-06-10"));
+        day.add(LocalDate.parse("2023-06-15"));
+
+        // Find the earliest date
+        LocalDate earlydate = day.stream()
+                .min(LocalDate::compareTo)
+                .orElse(null);
+
+        // Find the latest date
+        LocalDate latestDate = day.stream()
+                .max(LocalDate::compareTo)
+                .orElse(null);
+
+        // Print the earliest and latest day
+        System.out.println("Earliest Date: " + earlydate);
+        System.out.println("Latest Date: " + latestDate);
+    }
+}
